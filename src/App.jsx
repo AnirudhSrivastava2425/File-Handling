f6e13bec29cd7841467ed4a37a2bd859c0f6f11e
AnirudhSrivastava2425 <anirudhsrivastava113114@gmail.com>
2026-02-23 11:40:40 +0530

    Css for delete images updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,36 +8,38 @@ function App() {
 
   const [file, setFile] = useState([])
 
-  const filesUpload = (files) =>{
+  const filesUpload = (files) => {
     let filesArr = files.map((elem) => ({
       elem,
       preview: URL.createObjectURL(elem)
     }))
 
-    setFile((prev) => [...prev,...filesArr])
+    setFile((prev) => [...prev, ...filesArr])
   }
 
-  const handleFileDelete = (index) =>{
-    let updatedFiles = file.filter((_,i)=>i!==index)
+  const handleFileDelete = (index) => {
+    let updatedFiles = file.filter((_, i) => i !== index)
     setFile(updatedFiles)
   }
   return (
     <>
 
-    <Modal status={status} closeMethod={()=>setStatus(false)}>
-      <FileUpload status={status} closeMethod={()=>setStatus(false)}  dropMethod={filesUpload}/>
-    </Modal>
+      <Modal status={status} closeMethod={() => setStatus(false)}>
+        <FileUpload status={status} closeMethod={() => setStatus(false)} dropMethod={filesUpload} />
+      </Modal>
 
-    <h1>File Management | Drag-n-Drop</h1>
-    <p>Let's look into how to create a file drag-n-drop modal which is reusable across our application.</p>
-    <br />
-      <button onClick={()=>setStatus(true)}>Upload Files</button>
-      <br/>
-      <br/>
+      <h1>File Management | Drag-n-Drop</h1>
+      <p>Let's look into how to create a file drag-n-drop modal which is reusable across our application.</p>
+      <br />
+      <button onClick={() => setStatus(true)}>Upload Files</button>
+      <br />
+      <br />
       <div className="preview-img">
-      {file[0]?file.map((elem,index) =>(
-        <img key={elem.name} className='preview-images' onClick={()=>handleFileDelete(index)} src={elem.preview} alt={elem.name} />
-      )):"No files added."}</div>
+        {file[0] ? file.map((elem, index) => (
+          <div className="preview-wrapper">
+            <img key={elem.name} className='preview-images' onClick={() => handleFileDelete(index)} src={elem.preview} alt={elem.name} />
+          </div>
+        )) : "No files added."}</div>
     </>
   )
 }
